863b8303b4af418f970f8bc2430b6ac7c91aaa0c
Guillermo Brachetta <brachetta@me.com>
2020-05-03 03:16:50 +0200

    Changed message for not authorised

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = "no seas pelotudo!"
+login.login_message = "You cannot access this page. Please log in."
 login.login_message_category = 'info'
 app.config['MONGO_URI'] = Config.MONGO_URI
 mongo = PyMongo(app)
