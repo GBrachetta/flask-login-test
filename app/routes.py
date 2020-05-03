db33ffe7ddf0296bea4bc92459d8e6a4c449a4fd
Guillermo Brachetta <brachetta@me.com>
2020-05-03 03:21:15 +0200

    Added account route (not updating username)

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from app import app, mongo
 from app.users import User
 from bson import ObjectId
-from app.forms import RegistrationForm, LoginForm
+from app.forms import RegistrationForm, LoginForm, UpdateAccountForm
 
 
 @app.route('/')
@@ -20,10 +20,23 @@ def about():
     return render_template('about.html', title="About")
 
 
-@app.route('/account')
+@app.route('/account', methods=['POST', 'GET'])
 @login_required
 def account():
-    return render_template('account.html', title="About")
+    form = UpdateAccountForm()
+    updated_user = {'username': form.username.data, 'first_name': form.first_name.data,
+                    'last_name': form.last_name.data, 'email': form.email.data}
+    if form.validate_on_submit():
+        mongo.db.users.update_one(
+            {'_id': current_user._id}, {"$set": updated_user})
+        flash('Updated!', 'info')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title="About", form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
@@ -34,12 +47,12 @@ def login():
     if form.validate_on_submit():
         user = mongo.db.users.find_one({'email': form.email.data})
         if user and User.check_password(user['password'], form.password.data):
-            user_obj = User(user['username'], user['email'],
+            user_obj = User(user['username'], user['first_name'], user['last_name'], user['email'],
                             user['_id'], user['is_admin'])
             login_user(user_obj, remember=form.remember.data)
             next_page = request.args.get('next')
             flash('You have logged in!', 'info')
-            return redirect(next_page) if next_page else redirect('index')
+            return redirect(next_page) if next_page else redirect(url_for('index'))
         else:
             flash('Please check your credentials', 'warning')
     return render_template('login.html', title='Login', form=form)
@@ -53,8 +66,8 @@ def register():
     users = mongo.db.users
     if form.validate_on_submit():
         hashed_password = generate_password_hash(form.password.data)
-        users.insert({'username': form.username.data,
-                      'email': form.email.data, 'password': hashed_password, 'is_admin': False})
+        users.insert({'username': form.username.data, 'first_name': form.first_name.data,
+                      'last_name': form.last_name.data, 'email': form.email.data, 'password': hashed_password, 'is_admin': False})
         flash(f'Account created for {form.username.data}', 'info')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
